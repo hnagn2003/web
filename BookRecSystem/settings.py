b266b29f8f79d7aaff763527f3381ecf20b0be0b
nhngan <hnagn2003@gmail.com>
2023-05-04 22:58:43 +0700

    deploy to railway

--- BookRecSystem/settings.py
+++ BookRecSystem/settings.py
@@ -35,11 +35,13 @@ ALLOWED_HOSTS = [
     "kitabe.up.railway.app",
     "127.0.0.1",
     "localhost",
+    "web-production-ac16.up.railway.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://kitabe-app.herokuapp.com",
     "https://kitabe.up.railway.app",
+    "https://web-production-ac16.up.railway.app",
 ]
 
 
